d7e5a1e5f2c61e6841951cf96d029fc25297e7d2
Dirk Gadsden <dirk@esherido.com>
2015-09-07 16:59:26 -0700

    Refactor tests; add additional parsing tests

--- src/jstree/jstree_test.go
+++ src/jstree/jstree_test.go
@@ -1,5 +1,6 @@
 package jstree
 
+import _ "fmt"
 import "log"
 import "os"
 import "path"
@@ -17,6 +18,21 @@ func pathRelativeToPackageRoot(p string) string {
 	return path.Join(dir, "..", "..", p)
 }
 
+var __parsedProgram *Program = nil
+
+func getParsedProgram() *Program {
+	if __parsedProgram != nil { return __parsedProgram }
+
+	program, err := ParseFile(pathRelativeToPackageRoot("test/jstree_test.js"))
+	if err != nil { log.Fatal(err) }
+
+	__parsedProgram = program
+	return __parsedProgram
+}
+
+
+// Tests ---------------------------------------------------------------------
+
 func TestParseFile(t *testing.T) {
 	file := pathRelativeToPackageRoot("test/jstree_test.js")
 
@@ -26,9 +42,34 @@ func TestParseFile(t *testing.T) {
 
 func TestParseProgram(t *testing.T) {
 	require := require.New(t)
+	program := getParsedProgram()
 
-	program, err := ParseFile(pathRelativeToPackageRoot("test/jstree_test.js"))
-	require.NoError(err)
+	require.Equal(3, len(program.body))
+}
 
-	require.Equal(2, len(program.body))
+func TestParseVariableDeclaration(t *testing.T) {
+	require := require.New(t)
+	program := getParsedProgram()
+
+	// Expect `var ...` and `const ...` to be variable delcarations.
+	for i := 0; i <= 1; i++ {
+		variableDeclaration    := program.body[i]
+		nilVariableDeclaration := (*VariableDeclaration)(nil)
+		require.IsType(nilVariableDeclaration, variableDeclaration)
+	}
+
+	varDeclaration := (program.body[0]).(*VariableDeclaration)
+	require.Equal("var", varDeclaration.kind)
+
+	constDeclaration := (program.body[1]).(*VariableDeclaration)
+	require.Equal("const", constDeclaration.kind)
+}
+
+func TestParseFunctionDeclaration(t *testing.T) {
+	require := require.New(t)
+	program := getParsedProgram()
+
+	functionDeclaration    := program.body[2]
+	nilFunctionDeclaration := (*FunctionDeclaration)(nil)
+	require.IsType(nilFunctionDeclaration, functionDeclaration)
 }
--- test/jstree_test.js
+++ test/jstree_test.js
@@ -1,3 +1,7 @@
 var a = 'b'
 
 const c = 1 + 2
+
+function d(e) {
+	return f
+}
